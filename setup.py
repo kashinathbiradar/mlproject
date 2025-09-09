4ef54e5ba93ded92e90ec317a6e163c1438fc8e5
kashinathbiradar <biradarkashi00@gmail.com>
2025-09-10 02:50:23 +0530

    setup commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+
+HY='-e .'
+def get_requirements(file_path:str)->list[str]:
+
+ requirements=[]
+
+ with open(file_path) as file_obj:
+    requirements=file_obj.readlines()
+    requirements=[req.replace("\n"," ") for req in requirements]
+
+    if HY in requirements:
+      requirements.remove(HY)
+
+ return requirements
+
+
+
+setup(
+
+name='mlproject',
+version='0.0.1',
+author='kashinath Biradar',
+author_email='biradarkashi00@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'),
+
+
+)
